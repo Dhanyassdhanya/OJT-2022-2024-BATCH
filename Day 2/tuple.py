20c4e050dc2f2eaae09fb07e4c85bf4f3a439320
Dhanyassdhanya <122439840+Dhanyassdhanya@users.noreply.github.com>
2024-05-17 14:19:10 +0530

    Add files via upload

---
+++ Day 2/tuple.py
@@ -0,0 +1,23 @@
+# Convert a tuple to a list
+
+tuple=(2,3,4,5)
+my_list=list(tuple)
+print(my_list)
+
+# Create a tuple and access its elements.
+my_tuple = (1, 2, 3, 4, 5)
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+
+
+#  Concatenate two tuples
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concatenated_tuple = tuple1 + tuple2
+print("Concatenated tuple:", concatenated_tuple)
+
+
+#  Find the length of a tuple
+ftuple = (10, 20, 30, 40, 50)
+length = len(ftuple)
+print("Length of the tuple:", length)
