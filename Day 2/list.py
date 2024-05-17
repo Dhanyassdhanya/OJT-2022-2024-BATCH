6c48f676f7f58f14aee5e09bf59effd493944919
Dhanyassdhanya <122439840+Dhanyassdhanya@users.noreply.github.com>
2024-05-17 12:57:20 +0530

    Add files via upload

---
+++ Day 2/list.py
@@ -0,0 +1,5 @@
+# Remove an element from a list by value.
+
+list=[1,2,3,4]
+list.remove(3)
+print(list)
---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+#  Iterate over keys and values in a dictionary. 
+
+names={"fname:" : "Dhanya","age:":25}
+for a,b in names.items():
+    print(a,b)
