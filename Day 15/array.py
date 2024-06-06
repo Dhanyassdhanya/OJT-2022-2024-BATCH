e138111156646799f2ef9e9ea5574e86912b7ff4
Dhanyassdhanya <122439840+Dhanyassdhanya@users.noreply.github.com>
2024-06-06 17:04:58 +0530

    Add files via upload

---
+++ Day 15/array.py
@@ -0,0 +1,12 @@
+# Create the original array
+original_array = [1, 2, 3, 4, 5]
+
+# Make a copy of the original array
+copied_array = original_array.copy()
+
+# Change the original array
+original_array[0] = 99
+
+# Display both arrays
+print("Original array:", original_array)
+print("Copied array:", copied_array)
