e4c5b834bac2e442a8d53feb88ccbc557fea1b72
Dhanyassdhanya <122439840+Dhanyassdhanya@users.noreply.github.com>
2024-07-01 16:57:09 +0530

    Add files via upload

---
+++ Day 31/for_loop.py
@@ -0,0 +1,25 @@
+# List
+list=['Apple','Orange','Jerry','Banana'] 
+print("This is our list ",list) 
+i=1 
+for val in list: 
+    print("List Item",i,"is",val)
+
+# Tuple
+tuple=('Apple','Orange','Jerry','Banana')
+print("This is our tuple ",tuple)
+i=1
+for val in tuple:
+    print("Tuple Item",i,"is",val)
+
+# String
+str="Hello World"
+print("This is our string ",str)
+i=1
+for val in str:
+    print("String Item",i,"is",val)
+
+# Range
+for i in range(1,10):
+    print(i)
+    
